d941c5053f7679875c3411e0166bce3ebca6f4bf
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2020-02-17 01:06:44 +0530

    Assignment6-Question7

---
+++ Assignment6/Question7.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int min(int x, int y) {
+	return x < y ? x : y;
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    scanf("%d", &n);
+    int ar[n][n];
+    for (int x = 0; x < n; x++) {
+    	for(int y = 0; y < n; y++) {
+    		ar[x][y] = 10000000;
+    		if(x == y)
+    			ar[x][y] = 0;
+    	}
+    }
+
+    for(int x = 0; x < n-1; x++) {
+    	int a, b, c;
+    	scanf("%d %d %d", &a, &b, &c);
+    	ar[a][b] = c;
+    	ar[b][a] = c;
+    }
+
+    int r, k;
+    scanf("%d%d", &r, &k);
+
+    for(int x = 0; x < n; x++) {
+    	for(int y = 0; y < n; y++) {
+    		for(int z = 0; z < n; z++) {
+    			ar[y][z] = min(ar[y][z], ar[y][x] + ar[x][z]);
+    			ar[z][y] = ar[y][z];
+    		}
+    	}
+    }
+
+    for(int x = 0; x < n; x++) {
+    	if(ar[r][x] <= k) {
+    		// print only the leaf nodes
+    		printf("%d ", x);
+    	}
+    }
+}
